bf657d3af44e162cc7e88bfcceaa84ae191eda9b
sx14 <1059363093@qq.com>
2018-01-04 15:01:10 +0800

    path

--- main.m
+++ main.m
@@ -1,7 +1,8 @@
-video_base_path = '/home/sunx/dataset/ImageNet/train';
-output_path = '/home/sunx/output/VEB';
-annotation_base_path = '/home/sunx/dataset/ImageNet/Annotations';
+clear;clc;close all;
+video_base_path = '/media/magus/新加卷/ImageNet/train';
+output_path = '/home/magus/sunx/output/VEB';
+annotation_base_path = '/media/magus/新加卷/ImageNet/Annotations';
 video_list = load('video_list.mat');
 video_list = video_list.video_list;
-[recall, smT_IoU] = run([video_base_path,'/chosen0/'],'ILSVRC2015_train_00001000',[annotation_base_path,'/ILSVRC2015_VID_train_0000/'], output_path);
-% run_all(video_base_path,video_list,output_path,annotation_base_path)
+% [recall, smT_IoU] = run([video_base_path,'/chosen0/'],'ILSVRC2015_train_00001000',[annotation_base_path,'/ILSVRC2015_VID_train_0000/'], output_path);
+run_all(video_base_path,video_list,output_path,annotation_base_path)
--- run_all.m
+++ run_all.m
@@ -1,7 +1,7 @@
 function run_all(video_base_path,package_video,output_path,annotation_base_path)
-matlabpool 4;
-parfor i = 1:size(package_video,1)
+% matlabpool 4;
+for i = 2:2:size(package_video,1)
     video = package_video{i};
     run(video_base_path,video.video_dir,annotation_base_path,output_path);
 end
-matlabpool close;
+% matlabpool close;
